31f93225db4f8d21bd680425418c9bf75562d77a
justKMM <khaiminhmai2@gmail.com>
2025-11-15 12:36:02 +0200

    add sample data

---
+++ backend/data/battery_birth_certificate.py
@@ -0,0 +1,23 @@
+BATTERY_BIRTH_CERTIFICATE = {
+    "credentialSubject": {
+        "type": "BBC",
+        "batteryId": "did:itn:NiHW21TcdTkW8zk6ruhpfv",
+        "packUniqueId": "urn:uuid:98b46d2b-f71a-4bbc-b452-f1d2cdfee51c",
+        "manufacturersId": "did:itn:xrFroR8JhZBLbx9mLQ12S",
+        "manufacturingDate": "2024-04-15T18:38:28.576Z",
+        "manufacturingLocation": "Aichi, Japan",
+        "intendedUse": "Electric Vehicle 43",
+        "packWeight": "420",
+        "lifeCycleStatus": "IN USE",
+        "cellType": "Lithium-Ion",
+        "cellManufacturerId": "did:itn:example:cell-manufacturer",
+        "mobileOrStationary": "Mobile",
+        "bmsSoftwareVersion": "1.0.0",
+        "finalQualityTestDate": "2024-04-15T18:38:28.576Z",
+        "finalQualityTesterName": "Tanaka",
+        "packSerialNumber": "urn:uuid:8c99fee2-2bca-4d76-866b-4f465980be70",
+        "oemPartNumber": "1.23456789e+09",
+        "packDimensions": "100x100x100",
+    }
+}
+
---
+++ backend/data/battery_soh.py
@@ -0,0 +1,35 @@
+BATTERY_SOH_RECORDS = [
+    {
+        "credentialSubject": {
+            "type": "BatterySOH",
+            "batteryId": "did:itn:883c83bd37b342a9b8dda5",
+            "packUniqueId": "urn:uuid:97eff5ad-d828-4f14-a7d6-555fe918c64d",
+            "previousSOHVal": "93.0",
+            "previousSOHTimeStamp": "2025-04-18T23:28:49.481298Z",
+            "newSOHVal": "94.2",
+            "newSOHTimeStamp": "2025-05-06T18:07:49.481277Z",
+            "chargeCycles": "834",
+            "impedance": "5.52",
+            "maxCapacity": "74.7",
+            "batteryAge": "P2Y6M",
+            "_autoCorrected": True,
+        }
+    },
+    {
+        "credentialSubject": {
+            "type": "BatterySOH",
+            "batteryId": "did:itn:4c622a42593f4763b0a7a8",
+            "packUniqueId": "urn:uuid:72cbfea6-6c3a-4ec6-9751-35230764a80a",
+            "previousSOHVal": "90.19999999999999",
+            "previousSOHTimeStamp": "2025-06-06T14:39:49.481699Z",
+            "newSOHVal": "92.1",
+            "newSOHTimeStamp": "2025-06-06T19:48:49.481686Z",
+            "chargeCycles": "676",
+            "impedance": "5.44",
+            "maxCapacity": "73.6",
+            "batteryAge": "P6Y6M",
+            "_autoCorrected": True,
+        }
+    },
+]
+
